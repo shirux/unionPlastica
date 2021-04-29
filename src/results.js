e170bfff1781738dcbaa70af64c48483f1f44466
shirux <shirux123@gmail.com>
2021-04-29 15:57:00 -0500

    finish with results file

--- src/results.js
+++ src/results.js
@@ -1,13 +1,21 @@
 const config = require('../data/config');
 const fs = require('fs');
 
+/**
+ * Registra los resultados en un archivo csv, ya sea caso exitoso o un error.
+ * Esto para que el programa contable entienda los resultados
+ * @param {string} envios Envios realizados 
+ * @param {*} error Error
+ */
 const registerResult = async (envios, error = null) => {
     try {
         const resultFolder = `${config.files.rootFolder}/${config.files.resultFolder}`;
         let resultado = ''
-        if (error) {
+        
+        // Caso de error
+        if (error) { 
             resultado = `1; ${error.status}`
-        } else {
+        } else { // Caso Exito
             resultado = `0`
             if (envios) {
                 resultado += `; ${envios}`
